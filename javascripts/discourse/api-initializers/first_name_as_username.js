f6baa69c1f84dd71ea53e6a0e4b457df210747b9
celso-na-teixeira <65625371+celso-na-teixeira@users.noreply.github.com>
2024-03-02 12:44:33 +0000

    Update first_name_as_username.js

--- javascripts/discourse/api-initializers/first_name_as_username.js
+++ javascripts/discourse/api-initializers/first_name_as_username.js
@@ -1,39 +1,56 @@
 import { apiInitializer } from "discourse/lib/api";
 import User from "discourse/models/user";
-import discourseDebounce from "discourse-common/lib/debounce";
-import { observes } from "discourse-common/utils/decorators";
 
 export default apiInitializer("1.8.0", (api) => {
-    api.modifyClass("component:modal/create-account", {
-        didInsertElement: function(){
-             if (this.get("nameValidation.ok")) {
-                 
-  const name = this.accountName.trim().split(/\s/)[0];
-  
-  if (name.length) {
-    // Assuming User.checkUsername returns a Promise
-    User.checkUsername(name, this.accountEmail)
-      .then((result) => {
-        if (result.suggestion) {
-          this.setProperties({
-            accountUsername: result.suggestion,
-            prefilledUsername: result.suggestion,
-          });
-        } else {
-          this.setProperties({
-            accountUsername: name,
-            prefilledUsername: name,
-          });
-        }
-      })
-      .catch((error) => {
-        // Handle error if needed
-        console.error(error);
-      });
-  }
-}
+  const siteSettings = api.container.lookup("service:site-settings");
 
-            
-        }     
-    });
+  if (
+    !siteSettings.full_name_required ||
+    !siteSettings.prioritize_username_in_ux
+  ) {
+    return;
+  }
+
+  // Removes the number at the end of string
+  api.formatUsername((username) => {
+    return username.replace(/\d+$/, "");
+  });
+
+  // Prefilling the username based on the first name
+  api.modifyClass("component:modal/create-account", {
+    pluginId: "display-first-name-only",
+
+    prefillUsername() {
+      // do nothing.
+    },
+
+    prefillAndFormatUsername() {
+      if (this.prefilledUsername) {
+        if (this.model.accountUsername === this.prefilledUsername) {
+          this.set("model.accountUsername", "");
+        }
+        this.set("prefilledUsername", null);
+      }
+
+      if (this.get("nameValidation.ok")) {
+        const name = this.accountName.trim().split(/\s/)[0];
+        if (!name.length) {
+          return;
+        }
+
+        // Format the username (replace numbers at the end)
+        const formattedUsername = api.formatUsername(name);
+
+        // Validate and set the username
+        User.checkUsername(formattedUsername, this.accountEmail).then(
+          (result) => {
+            this.setProperties({
+              accountUsername: result.suggestion || formattedUsername,
+              prefilledUsername: result.suggestion || formattedUsername,
+            });
+          }
+        );
+      }
+    },
+  });
 });
