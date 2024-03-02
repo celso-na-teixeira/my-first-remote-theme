246b38166be9db5b4ae622e6cf45041cac9eba13
celso-na-teixeira <65625371+celso-na-teixeira@users.noreply.github.com>
2024-03-02 14:32:59 +0000

    Update first_name_as_username.js

--- javascripts/discourse/api-initializers/first_name_as_username.js
+++ javascripts/discourse/api-initializers/first_name_as_username.js
@@ -1,5 +1,7 @@
 import { apiInitializer } from "discourse/lib/api";
 import User from "discourse/models/user";
+import discourseDebounce from "discourse-common/lib/debounce";
+import { observes } from "discourse-common/utils/decorators";
 
 export default apiInitializer("1.8.0", (api) => {
   const siteSettings = api.container.lookup("service:site-settings");
