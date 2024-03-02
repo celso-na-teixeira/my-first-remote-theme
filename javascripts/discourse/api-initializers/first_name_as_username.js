2e96d018cda6217a1252cd7faedb1a7aa8b07c35
celso-na-teixeira <65625371+celso-na-teixeira@users.noreply.github.com>
2024-03-02 11:34:57 +0000

    Update first_name_as_username.js

--- javascripts/discourse/api-initializers/first_name_as_username.js
+++ javascripts/discourse/api-initializers/first_name_as_username.js
@@ -1,69 +1,14 @@
-import EmberObject from "@ember/object";
-import Mixin from "@ember/object/mixin";
-import { isEmpty } from "@ember/utils";
-import { setting } from "discourse/lib/computed";
+import { apiInitializer } from "discourse/lib/api";
 import User from "discourse/models/user";
 import discourseDebounce from "discourse-common/lib/debounce";
-import discourseComputed from "discourse-common/utils/decorators";
-import I18n from "discourse-i18n";
+import { observes } from "discourse-common/utils/decorators";
 
-
-export default Mixin.create({
-  checkedUsername: null,
-  usernameValidationResult: null,
-  uniqueUsernameValidation: null,
-  maxUsernameLength: setting("max_username_length"),
-  minUsernameLength: setting("min_username_length"),
-
-  async fetchExistingUsername() {
-    const result = await User.checkUsername(null, this.accountEmail);
-    console.log(result);
-    if (
-      result.suggestion &&
-      (isEmpty(this.accountUsername) ||
-        this.accountUsername === this.get("authOptions.username"))
-    ) {
-      this.setProperties({
-        accountUsername: result.suggestion,
-        prefilledUsername: result.suggestion,
-      });
-    }
-  },
-
-  @discourseComputed(
-    "usernameValidationResult",
-    "accountUsername",
-    "forceValidationReason"
-  )
-  usernameValidation() {
-    if (
-      this.usernameValidationResult &&
-      this.checkedUsername === this.accountUsername
-    ) {
-      return this.usernameValidationResult;
-    }
-
-    
-
-    if (result.shouldCheck) {
-      discourseDebounce(this, this.checkUsernameAvailability, 500);
-    }
-
-    return result;
-  },
-
-
-  async checkUsernameAvailability() {
-    const result = await User.checkUsername(
-      this.accountUsername,
-      this.accountEmail
-    );
-
-    if (this.isDestroying || this.isDestroyed) {
-      return;
-    }
-
-    this.set("checkedUsername", this.accountUsername);
-    this.set("isDeveloper", !!result.is_developer);
-  },
+export default apiInitializer("1.8.0", (api) => {
+    api.modifyClass("component:modal/create-account", {
+        didInsertElement: function(){
+            var nameValue = document.getElementById('new-account-name').value;
+            var formattedName = nameValue.split(' ').slice(0, -1).join(' ');
+            document.getElementById('new-account-username').value = formattedName;
+        }     
+    });
 });
